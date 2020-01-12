c63f839404a78aa9aed3a4c911111c8d5ecc8c86
aliefamzari <aliefamzari@gmail.com>
2020-01-12 10:34:51 +0800

    add new Exchange script

---
+++ CheckRecipientTypeDetailsWithOutput.ps1
@@ -0,0 +1,17 @@
+﻿#This script is to fetch RecepientTypeDetails properties and output result to CSV file. The script read user array from userlist.csv that you supplied.
+#Place userlist.csv at your desktop
+#Place CheckRecipientTypeDetailsWithOutput.ps1 at your desktop
+#Output file will save as out.csv at your desktop 
+#All rights reserved AAMA 2019
+
+$File = "$env:USERPROFILE\Desktop\userlist.csv"   
+$userlist = Import-CSV -Path $File
+$results = @()
+
+foreach ($user in $userlist){
+$results += Get-Recipient $user.namelist -ErrorAction SilentlyContinue |Select Name, RecipientTypeDetails
+}
+
+Write-Output $results         
+
+$results | Export-Csv $env:USERPROFILE\Desktop\out.csv -NoTypeInformation  
