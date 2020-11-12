618235d45f9894265d38fbe622a9b7fc40c5dcb0
aliefamzari <aliefamzari@gmail.com>
2020-11-12 10:10:32 +0800

    wip

--- SpringCleaning.ps1
+++ SpringCleaning.ps1
@@ -4,6 +4,7 @@
 #Start-Transcript
 #Sourcing some functoin
 . ".\function_notification.ps1"
+. ".\function_DeleteEmptyFolder.ps1"
 
 $ErrorActionPreference = "SilentlyContinue"
 filter timestamp {"$(Get-Date -Format o)"}
@@ -26,21 +27,22 @@ $audio = Get-ChildItem -Recurse $SourcePath* -Include *.aac, *.mp3, *.flac, *.m4
 $programs = Get-ChildItem -Recurse $SourcePath* -Include *.exe, *.msi 
 $zipped = Get-ChildItem -Recurse $SourcePath* -Include *.zip, *.rar, *.7z 
 $documents = Get-ChildItem -Recurse $SourcePath* -Include *.pdf, *.doc, *.docx, *.xls, *.xlsx, *.odt 
-
+$images = Get-ChildItem -Recurse $SourcePath* -Include *.jpg, *.jpeg, *.png, *.webm
 
 #Doing some math stuff
-$filetypecount = Get-ChildItem -file -Recurse $SourcePath\* |ForEach-Object {$_.Name.Split('.')[-1]} |Group-Object |Select-Object Name, count |Sort-Object count -Descending #>> $logfile
-$totalcatchfiles = $audio.count + $videos.count + $zipped.Count + $documents.Count + $programs.count
+$filetypecount = Get-ChildItem -file -Recurse $SourcePath\* |ForEach-Object {$_.Name.Split('.')[-1]} |Group-Object |Select-Object Name, count |Sort-Object count -Descending 
+$totalcatchfiles = $audio.count + $videos.count + $zipped.Count + $documents.Count + $programs.count + $images.count
 $totalfilesize = (($zipped | Measure-Object -sum length).Sum)/1gb + `
                 (($audio | Measure-Object -sum length).Sum)/1gb + `
                 (($programs | Measure-Object -sum length).Sum)/1gb + `
                 (($zipped | Measure-Object -sum length).Sum)/1gb + `
                 (($documents | Measure-Object -sum length).Sum)/1gb +`
-                (($videos | Measure-Object -sum Length).Sum)/1gb
+                (($videos | Measure-Object -sum Length).Sum)/1gb +`
+                (($images | Measure-Object -sum Length).Sum)/1gb
 $totalgb = [math]::Round($totalfilesize,2)
 
 #tossing stuff into a log file
-$videos, $audio, $programs, $zipped, $documents >> $tlog
+$videos, $audio, $programs, $zipped, $documents, $images >> $tlog
 Write-Output "" >> $tlog
 Write-Output "File type count" >> $tlog 
 $filetypecount | Out-File -Append $tlog
@@ -60,32 +62,41 @@ $UserInput = [System.windows.Forms.Messagebox]::Show("Would you like to move the
 #Doing the actual move. Serious stuff going on. 
 switch ($UserInput) {
     Yes {
-            # #[WIP]fixing destination path when move, missing original subfolder 
-            # $videos | ForEach-Object {
-            #     $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($source.Length)
-            #     New-Item $newpath -Type Directory 
-            #     move-item -Force $_.FullName -Destination $newpath
-            #     }
-            # $audio | ForEach-Object {
-            #     $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($source.Length)
-            #     New-Item $newpath -Type Directory 
-            #     move-item -Force $_.FullName -Destination $newpath
-            #     }
-            # $programs | ForEach-Object {
-            #     $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($source.Length)
-            #     New-Item $newpath -Type Directory 
-            #     move-item -Force $_.FullName -Destination $newpath
-            #     }
-            # $zipped | ForEach-Object {
-            #     $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($source.Length)
-            #     New-Item $newpath -Type Directory
-            #     move-item -Force $_.FullName -Destination $newpath
-            #     }
-            # $documents | ForEach-Object {
-            #     $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($source.Length)
-            #     New-Item $newpath -Type Directory 
-            #     move-item -Force $_.FullName -Destination $newpath
-            #     }
+            #[WIP]fixing destination path when move, missing original subfolder 
+            $videos | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory 
+                move-item -Force $_.FullName -Destination $newpath
+            }             
+            $audio | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory 
+                move-item -Force $_.FullName -Destination $newpath
+            }
+            $programs | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory 
+                move-item -Force $_.FullName -Destination $newpath
+            }
+            $zipped | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory
+                move-item -Force $_.FullName -Destination $newpath
+            }
+            $documents | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory 
+                move-item -Force $_.FullName -Destination $newpath
+            }
+            $images | ForEach-Object {
+                $newpath = Join-Path $DestinationPath $_.DirectoryName.Substring($SourcePath.Length)
+                New-Item $newpath -Type Directory
+                move-item -Force $_.FullName -Destination $newpath
+            }
+            
+            #Clean empty folders
+            Delete-EmptyFolder -path $SourcePath
+
     }
     No {
         Show-Notification -ToastTitle "Spring Cleaning Cancelled" -ToastText "No files were moved"
