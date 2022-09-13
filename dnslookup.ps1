3546691c7188ec5e6ac871dae30bcf39c14291a7
aliefamzari <aliefamzari@gmail.com>
2022-09-13 21:30:49 +0800

    fix csv spacing

--- dnslookup.ps1
+++ dnslookup.ps1
@@ -2,7 +2,7 @@
 
 
 
-$csvheader = '"Time(UTC)","ResolvedORFailed","FQDN"'
+$csvheader = '"Time(UTC)","FQDN","ResolvedORFailed"'
 $outdir = "c:\hp"
 $currentime = (get-date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss")
 $fqdn = 'itsupport.orsted.com'
@@ -11,10 +11,10 @@ function write-header {
     $csvheader |Out-File $outdir\result.csv
 }
 
-# $testfile = Test-Path $outdir\result.csv
-# if (!$testfile) {
-#     write-header
-# }
+$testfile = Test-Path $outdir\result.csv
+if (!$testfile) {
+    write-header
+}
 
 $result = @()
 
@@ -23,7 +23,10 @@ Try{
     $dnsa = Resolve-DnsName $fqdn -ErrorAction Stop |Select-Object ipaddress |Format-List |out-string
     $dnsa = $dnsa.split(':') |Select-Object -Index 1 
     $dnsa = $dnsa -replace "`n| ",""
-    write-output "$currentime,$fqdn,$dnsa"
+    $trim = write-output "$currentime,$fqdn,$dnsa" |out-string
+    $trim.trim() | out-file -FilePath $outdir\result.csv -Append
+
+
   
 }
 Catch{
